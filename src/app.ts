78b6ff2a7c7cffb13789d8e1a7280aea9a47fee8
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-07-14 19:49:00 +0545

    app.ts / get added

--- src/app.ts
+++ src/app.ts
@@ -9,16 +9,28 @@ import orderRoute from "./route/orderRoute";
 import cartRoute from "./route/cartRoute";
 import cors from "cors";
 const app = express();
+
 app.use(
   cors({
     origin: "*",
-  }),
+  })
 );
+
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({
+    success: true,
+    message: "Shree Beauty Parlour API is running 🚀",
+  });
+});
+
 app.use("/api/", authRoute);
 app.use("/api/category", cateogryRoute);
 app.use("/api/product", productRoute);
 app.use("/api/orders", orderRoute);
 app.use("/api/cart", cartRoute);
+
 app.use(express.static("./src/uploads"));
+
 export default app;
