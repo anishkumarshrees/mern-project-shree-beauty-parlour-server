399da59f93ef0f49b0b45fbe293b7d600ffd2b44
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-07-14 20:12:04 +0545

    server.ts

--- server.ts
+++ server.ts
@@ -18,7 +18,7 @@ function startServer() {
   });
   const io = new Server(server, {
     cors: {
-      origin: ["http://localhost:5173", "http://localhost:3000"],
+      origin: "https://shree-beauty-parlour-six.vercel.app",
     },
   });
   let onlineUsers: {
