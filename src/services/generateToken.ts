d9f1cbbea0a96015de8609a4436e0647461aea5d
Anish Kumar Shrees <anishshrees12@gmail.com>
2026-07-16 17:27:25 +0545

    user role

--- src/services/generateToken.ts
+++ src/services/generateToken.ts
@@ -1,12 +1,22 @@
-import  jwt  from "jsonwebtoken"
-import user from "../database/models/user.model"
-import envConfig from "../config/config"
-import { StringValue } from "ms"
+import jwt from "jsonwebtoken";
+import user from "../database/models/user.model";
+import envConfig from "../config/config";
+import { StringValue } from "ms";
 
-const generateToken=(userId:string)=>{
-//token generation using jwt
-const token = jwt.sign({userId: userId }, envConfig.jwtsecretkey as string, { expiresIn : envConfig.jwtExpiresIn as string | number as StringValue })
-return token
-}
+const generateToken = (userData: user) => {
 
-export default generateToken
+  const token = jwt.sign(
+    {
+      userId: userData.id,
+      role: userData.role
+    },
+    envConfig.jwtsecretkey as string,
+    {
+      expiresIn: envConfig.jwtExpiresIn as StringValue
+    }
+  );
+
+  return token;
+};
+
+export default generateToken;
